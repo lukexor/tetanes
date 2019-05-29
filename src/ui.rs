4216ec9dd76b9ed23fa623efe31117e37799d984
Luke Petherbridge <lukexor@gmail.com>
2019-05-29 15:32:10 -0700

    Fixed turbo again

--- src/ui.rs
+++ src/ui.rs
@@ -7,6 +7,7 @@ use crate::util::Result;
 use failure::format_err;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::EventPump;
 use std::cell::RefCell;
 use std::path::PathBuf;
 use std::rc::Rc;
@@ -27,6 +28,7 @@ pub struct UI {
     paused: bool,
     sound_enabled: bool,
     state_slot: u8,
+    lctrl: bool,
 }
 
 impl UI {
@@ -43,6 +45,7 @@ impl UI {
             paused: false,
             sound_enabled: true,
             state_slot: 1u8,
+            lctrl: false,
         })
     }
 
@@ -75,70 +78,11 @@ impl UI {
         if let Some(slot) = load_slot {
             console.load_state(slot)?;
         }
-        let mut lctrl = false;
-        let mut turbo = false;
-        'running: loop {
-            turbo = console.cpu.mem.ppu.frame() % 6 < 3;
-            for event in event_pump.poll_iter() {
-                match event {
-                    Event::Quit { .. } => break 'running,
-                    Event::KeyDown {
-                        keycode: Some(key), ..
-                    } => match key {
-                        Keycode::Escape => self.paused = !self.paused,
-                        Keycode::LCtrl => lctrl = true,
-                        Keycode::O if lctrl => eprintln!("Open not implemented"), // TODO
-                        Keycode::Q if lctrl => break 'running,
-                        Keycode::R if lctrl => console.reset(),
-                        Keycode::P if lctrl => console.power_cycle(),
-                        Keycode::Equals if lctrl => {
-                            if self.speed < MAX_SPEED {
-                                self.speed += 25.0;
-                                console.set_speed(self.speed / DEFAULT_SPEED);
-                            }
-                        }
-                        Keycode::Minus if lctrl => {
-                            if self.speed > MIN_SPEED {
-                                self.speed -= 25.0;
-                                console.set_speed(self.speed / DEFAULT_SPEED);
-                            }
-                        }
-                        Keycode::Space => {
-                            self.fastforward = !self.fastforward;
-                            if self.fastforward {
-                                self.speed = MAX_SPEED;
-                            } else {
-                                self.speed = DEFAULT_SPEED;
-                            }
-                            console.set_speed(self.speed / DEFAULT_SPEED);
-                        }
-                        Keycode::Num1 if lctrl => self.state_slot = 1,
-                        Keycode::Num2 if lctrl => self.state_slot = 2,
-                        Keycode::Num3 if lctrl => self.state_slot = 2,
-                        Keycode::Num4 if lctrl => self.state_slot = 3,
-                        Keycode::S if lctrl => console.save_state(self.state_slot)?,
-                        Keycode::L if lctrl => console.load_state(self.state_slot)?,
-                        Keycode::M if lctrl => self.sound_enabled = !self.sound_enabled,
-                        Keycode::V if lctrl => eprintln!("Recording not implemented"), // TODO
-                        Keycode::D if lctrl => {
-                            self.debug = !self.debug;
-                            console.debug(self.debug);
-                        }
-                        Keycode::Return if lctrl => self.window.toggle_fullscreen(),
-                        Keycode::F10 => crate::util::screenshot(&console.render()),
-                        Keycode::F9 => eprintln!("Logging not implemented"), // TODO
-                        _ => self.handle_gamepad_event(&input, key, true, turbo),
-                    },
-                    Event::KeyUp {
-                        keycode: Some(key), ..
-                    } => match key {
-                        Keycode::LCtrl => lctrl = false,
-                        _ => self.handle_gamepad_event(&input, key, false, turbo),
-                    },
-                    _ => (),
-                }
+        loop {
+            let should_break = self.poll_events(&mut console, &input, &mut event_pump)?;
+            if should_break {
+                break;
             }
-
             if !self.paused {
                 let mut frames_to_run = (self.speed / DEFAULT_SPEED).floor() as usize;
                 if frames_to_run == 0 {
@@ -161,13 +105,97 @@ impl UI {
         Ok(())
     }
 
+    fn poll_events(
+        &mut self,
+        console: &mut Console,
+        input: &InputRef,
+        event_pump: &mut EventPump,
+    ) -> Result<bool> {
+        let turbo = console.cpu.mem.ppu.frame() % 6 < 3;
+        {
+            let mut input = input.borrow_mut();
+            if input.turboa {
+                input.gamepad1.a = turbo;
+            }
+            if input.turbob {
+                input.gamepad1.b = turbo;
+            }
+        }
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. } => return Ok(true),
+                Event::KeyDown {
+                    keycode: Some(key), ..
+                } => match key {
+                    Keycode::Escape => self.paused = !self.paused,
+                    Keycode::LCtrl => self.lctrl = true,
+                    Keycode::O if self.lctrl => eprintln!("Open not implemented"), // TODO
+                    Keycode::Q if self.lctrl => return Ok(true),
+                    Keycode::R if self.lctrl => console.reset(),
+                    Keycode::P if self.lctrl => console.power_cycle(),
+                    Keycode::Equals if self.lctrl => {
+                        if self.speed < MAX_SPEED {
+                            self.speed += 25.0;
+                            console.set_speed(self.speed / DEFAULT_SPEED);
+                        }
+                    }
+                    Keycode::Minus if self.lctrl => {
+                        if self.speed > MIN_SPEED {
+                            self.speed -= 25.0;
+                            console.set_speed(self.speed / DEFAULT_SPEED);
+                        }
+                    }
+                    Keycode::Space => {
+                        self.fastforward = !self.fastforward;
+                        if self.fastforward {
+                            self.speed = MAX_SPEED;
+                        } else {
+                            self.speed = DEFAULT_SPEED;
+                        }
+                        console.set_speed(self.speed / DEFAULT_SPEED);
+                    }
+                    Keycode::Num1 if self.lctrl => self.state_slot = 1,
+                    Keycode::Num2 if self.lctrl => self.state_slot = 2,
+                    Keycode::Num3 if self.lctrl => self.state_slot = 2,
+                    Keycode::Num4 if self.lctrl => self.state_slot = 3,
+                    Keycode::S if self.lctrl => console.save_state(self.state_slot)?,
+                    Keycode::L if self.lctrl => console.load_state(self.state_slot)?,
+                    Keycode::M if self.lctrl => self.sound_enabled = !self.sound_enabled,
+                    Keycode::V if self.lctrl => eprintln!("Recording not implemented"), // TODO
+                    Keycode::D if self.lctrl => {
+                        self.debug = !self.debug;
+                        console.debug(self.debug);
+                    }
+                    Keycode::Return if self.lctrl => self.window.toggle_fullscreen(),
+                    Keycode::F10 => crate::util::screenshot(&console.render()),
+                    Keycode::F9 => eprintln!("Logging not implemented"), // TODO
+                    _ => self.handle_gamepad_event(&input, key, true, turbo),
+                },
+                Event::KeyUp {
+                    keycode: Some(key), ..
+                } => match key {
+                    Keycode::LCtrl => self.lctrl = false,
+                    _ => self.handle_gamepad_event(&input, key, false, turbo),
+                },
+                _ => (),
+            }
+        }
+        Ok(false)
+    }
+
     fn handle_gamepad_event(&mut self, input: &InputRef, key: Keycode, down: bool, turbo: bool) {
         let mut input = input.borrow_mut();
         match key {
             Keycode::Z => input.gamepad1.a = down,
             Keycode::X => input.gamepad1.b = down,
-            Keycode::A => input.gamepad1.a = turbo && down,
-            Keycode::S => input.gamepad1.b = turbo && down,
+            Keycode::A => {
+                input.turboa = down;
+                input.gamepad1.a = turbo && down;
+            }
+            Keycode::S => {
+                input.turbob = down;
+                input.gamepad1.b = turbo && down;
+            }
             Keycode::RShift => input.gamepad1.select = down,
             Keycode::Return => input.gamepad1.start = down,
             Keycode::Up => input.gamepad1.up = down,
