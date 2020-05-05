1d0b06ed138fca3f6e833251d34de5d285bd2692
Luke Petherbridge <lukexor@gmail.com>
2020-05-05 16:43:51 -0700

    Added some webpack config

--- tetanes-web/www/webpack.common.js
+++ tetanes-web/www/webpack.common.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   entry: './bootstrap.js',
@@ -6,4 +7,7 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+  ]
 };
--- tetanes-web/www/webpack.prod.js
+++ tetanes-web/www/webpack.prod.js
@@ -4,6 +4,9 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
+  devServer: {
+    hot: false,
+  },
   output: merge(common.output, {
     publicPath: '/tetanes/dist/',
   }),
