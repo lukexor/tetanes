38dd7cb81be4ae824183378afc8aaf761914a95a
Lucas Petherbridge <lukexor@gmail.com>
2022-06-14 19:48:00 -0700

    fix: webpack

--- tetanes-web/www/webpack.config.js
+++ tetanes-web/www/webpack.config.js
@@ -17,8 +17,7 @@ module.exports = {
   },
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
+    path: path.resolve(__dirname, "public"),
   },
   experiments: {
     asyncWebAssembly: true,
