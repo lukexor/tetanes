6e5dfd02c3fd369877f7ed77718d33497c239100
DoubleGate <parobek@gmail.com>
2025-02-09 01:59:39 -0500

    Restructure Main for Readability and Performance

--- tetanes/src/main.rs
+++ tetanes/src/main.rs
@@ -24,26 +24,23 @@ use tetanes::{logging, nes::Nes};
 mod opts;
 
 fn main() -> anyhow::Result<()> {
-    let log = logging::init();
-    if let Err(err) = log {
+    // Initialize logging early and handle error immediately
+    if let Err(err) = logging::init() {
         eprintln!("failed to initialize logging: {err:?}");
     }
 
     #[cfg(feature = "profiling")]
     puffin::set_scopes_on(true);
 
-    Nes::run({
-        cfg_if! {
-            if #[cfg(target_arch = "wasm32")] {
-                tetanes::nes::config::Config::load(None)
-            } else {
-                use clap::Parser;
-
-                let opts = opts::Opts::parse();
-                tracing::debug!("CLI Options: {opts:?}");
-
-                opts.load()?
-            }
+    // Avoid unnecessary block expression
+    Nes::run(cfg_if! {
+        if #[cfg(target_arch = "wasm32")] {
+            tetanes::nes::config::Config::load(None)
+        } else {
+            use clap::Parser;
+            let opts = opts::Opts::parse();
+            tracing::debug!("CLI Options: {opts:?}");
+            opts.load()?
         }
     })
 }
