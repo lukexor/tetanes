f58b31cc9d27f0187d759796c64e34ccea3f784a
Luke Petherbridge <lukexor@gmail.com>
2025-03-13 17:24:20 -0700

    docs: ensure features display in docs.rs

--- tetanes/src/lib.rs
+++ tetanes/src/lib.rs
@@ -3,6 +3,7 @@
     html_favicon_url = "https://github.com/lukexor/tetanes/blob/main/assets/linux/icon.png?raw=true",
     html_logo_url = "https://github.com/lukexor/tetanes/blob/main/assets/linux/icon.png?raw=true"
 )]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
 
 pub mod error;
 pub mod logging;
