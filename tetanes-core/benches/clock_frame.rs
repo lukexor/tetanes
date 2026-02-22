a20dd836254fa9faff779323440916a2047b4653
Luke Petherbridge <lukexor@gmail.com>
2026-02-22 12:39:32 -0800

    chore: benchmark updates

--- tetanes-core/benches/clock_frame.rs
+++ tetanes-core/benches/clock_frame.rs
@@ -1,51 +1,70 @@
-use criterion::{Criterion, criterion_group, criterion_main};
-use pprof::criterion::{Output, PProfProfiler};
-use std::{fs::File, hint::black_box, path::Path, time::Duration};
-use tetanes_core::{
-    control_deck::{Config, ControlDeck},
-    mem::RamState,
-};
+#![allow(clippy::expect_used, reason = "fine in a benchmark")]
 
-fn clock_frames(rom_path: impl AsRef<Path>, frames: u32) {
-    let base_path = Path::new(env!("CARGO_MANIFEST_DIR"));
-    let rom_path = base_path.join(rom_path);
-    assert!(rom_path.exists(), "No test rom found for {rom_path:?}");
+use std::{
+    fs::File,
+    hint::black_box,
+    path::{Path, PathBuf},
+    time::Instant,
+};
+use tetanes_core::prelude::*;
+
+fn main() {
+    const FRAMES_TO_RUN: u32 = 400;
+    const ITERATIONS: u32 = 30;
+
+    let rom_path = std::env::args()
+        .find(|arg| arg.ends_with(".nes"))
+        .map(PathBuf::from)
+        .map(|path| {
+            if path.exists() {
+                path
+            } else {
+                // The working directory of every benchmark is set to the root directory of
+                // the package the benchmark belongs to.
+                //
+                // So if path is relative, it might be relative to the workspace not the package
+                // the benchmark is in
+                std::env::current_dir()
+                    .expect("valid cwd")
+                    .join("..")
+                    .join(path)
+            }
+        })
+        .unwrap_or_else(|| {
+            let base_path = Path::new(env!("CARGO_MANIFEST_DIR"));
+            base_path.join("test_roms/spritecans.nes")
+        });
+    let rom_path = rom_path.canonicalize().expect("valid rom path");
     let mut rom = File::open(&rom_path).expect("failed to open path");
+
     let mut deck = ControlDeck::with_config(Config {
         ram_state: RamState::AllZeros,
         ..Default::default()
     });
     deck.load_rom(rom_path.to_string_lossy(), &mut rom)
         .expect("failed to load rom");
-    while deck.frame_number() < frames {
-        deck.clock_frame().expect("valid frame clock");
-        deck.clear_audio_samples();
+
+    // Warmup
+    for _ in 0..3 {
+        deck.reset(ResetKind::Hard);
+        while deck.frame_number() < FRAMES_TO_RUN {
+            black_box(deck.clock_frame()).expect("valid frame clock");
+            deck.clear_audio_samples();
+        }
     }
-}
 
-fn basic(c: &mut Criterion) {
-    let mut group = c.benchmark_group("nes");
-    group.measurement_time(Duration::from_secs(60));
-    group.sample_size(10);
-    group.bench_function("basic", |b| {
-        b.iter(|| clock_frames("test_roms/ppu/_240pee.nes", black_box(200)))
-    });
-    group.finish();
-}
+    let start = Instant::now();
+    for _ in 0..ITERATIONS {
+        deck.reset(ResetKind::Hard);
+        while deck.frame_number() < FRAMES_TO_RUN {
+            black_box(deck.clock_frame()).expect("valid frame clock");
+            deck.clear_audio_samples();
+        }
+    }
+    let elapsed = start.elapsed().as_secs_f64();
 
-fn stress(c: &mut Criterion) {
-    let mut group = c.benchmark_group("nes");
-    group.measurement_time(Duration::from_secs(60));
-    group.sample_size(10);
-    group.bench_function("stress", |b| {
-        b.iter(|| clock_frames("test_roms/spritecans.nes", black_box(1000)));
-    });
-    group.finish();
+    let ms_per_frame = (elapsed / f64::from(FRAMES_TO_RUN * ITERATIONS)) * 1000.0;
+    println!("=== RESULTS ===");
+    println!("{elapsed:.2} s total");
+    println!("{ms_per_frame:.3} ms/frame");
 }
-
-criterion_group!(
-    name = benches;
-    config = Criterion::default().with_profiler(PProfProfiler::new(100, Output::Flamegraph(None)));
-    targets = basic, stress
-);
-criterion_main!(benches);
--- tetanes-utils/src/bin/test_known_games.sh
+++
@@ -1,2 +0,0 @@
-cargo build
-find ../roms/ -name '*nes' -exec "$CARGO_TARGET_DIR"/debug/tetanes -c {} \;
